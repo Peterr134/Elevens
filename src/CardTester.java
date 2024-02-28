a43bfc50a396d6ab534eb21919901f3a4d31b354
Peterr134 <peterr134@nycstudents.net>
2024-02-28 09:18:01 -0500

    Parts 1 and 2 done!

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class CardTester {
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        Card card1 = new Card("Ace", "Spades", 1);
+        Card card2 = new Card("4", "Hearts", 4);
+        Card card3 = new Card("10", "Clubs", 10);
+        System.out.println(card1.toString());
+        System.out.println(card1.matches(card2));
+        Deck deck = new Deck(new String[]{"Queen", "King", "Rook", "Bishop", "Knight", "Pawn"}, new String[]{"White", "Black"}, new int[]{1, 2, 3, 4, 5, 6});
+        System.out.println(deck.deal().toString());
+        System.out.println(deck.isEmpty());
+        System.out.println(deck.size());
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Deck {
+import java.util.ArrayList;
+
+public class Deck {
+    ArrayList<Card> deck;
+    int size;
+
+    public Deck(String[] names, String[] suits, int[] pointValues){
+        deck = new ArrayList<Card>();
+        for(int i = 0; i < suits.length; i++){
+            for(int j = 0; j < names.length; j++){
+                deck.add(new Card(names[j], suits[i], pointValues[j]));
+            }
+        }
+        size = deck.size();
+    }
+
+    public boolean isEmpty(){
+        return deck.size() == 0;
+    }
+
+    public int size(){
+        return deck.size();
+    }
+
+    public Card deal(){
+        if(deck.isEmpty()){
+            return null;
+        }else{
+            size--;
+            return deck.get(size);
+        }
+    }
 }
